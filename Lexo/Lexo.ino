16239ca14d57ca118d8e30057a47b07aa963c3e1
Edwin Zhang <ehzhang@mit.edu>
2014-02-25 16:43:27 -0500

    code for final presentation

--- Lexo/Lexo.ino
+++ Lexo/Lexo.ino
@@ -13,36 +13,42 @@ int SERVO_MAX_POS = 160,
     SERVO_PIN_RING = 12,
     SERVO_PIN_PINKY = 13;
     
+const int EXTD = SERVO_MAX_POS;
+const int ONEQ = 105; // 1/3 bent
+const int HALF = (SERVO_MAX_POS-SERVO_MIN_POS)/2;
+const int THRQ = 25; // 3/4 bent
+const int BENT = SERVO_MIN_POS;
 // Finger array!
 Servo fingers[5];
 
 int fingerPos[26][5] = {
-  {20, 90, 160, 90, 20}, // a
-  {90, 20, 20, 160, 90}, // b
-  {160, 10, 90, 90, 90}, // c
-  {90, 90, 90, 90, 90}, // d
-  {90, 90, 90, 90, 90}, // e
-  {90, 90, 90, 90, 90}, // f
-  {90, 90, 90, 90, 90}, // g
-  {90, 90, 90, 90, 90}, // h
-  {90, 90, 90, 90, 90}, // i
-  {90, 90, 90, 90, 90}, // j
-  {90, 90, 90, 90, 90}, // k
-  {90, 90, 90, 90, 90}, // l
-  {90, 90, 90, 90, 90}, // m
-  {90, 90, 90, 90, 90}, // n
-  {90, 90, 90, 90, 90}, // o
-  {90, 90, 90, 90, 90}, // p
-  {90, 90, 90, 90, 90}, // q
-  {90, 90, 90, 90, 90}, // r
-  {90, 90, 90, 90, 90}, // s
-  {90, 90, 90, 90, 90}, // t
-  {90, 90, 90, 90, 90}, // u
-  {90, 90, 90, 90, 90}, // v
-  {90, 90, 90, 90, 90}, // w
-  {90, 90, 90, 90, 90}, // x
-  {90, 90, 90, 90, 90}, // y
-  {90, 90, 90, 90, 90} // z 
+// THUMB INDEX MIDDL RING  PINKY
+  {THRQ, BENT, BENT, BENT, BENT}, // A
+  {BENT, EXTD, EXTD, EXTD, EXTD}, // B
+  {HALF, ONEQ, ONEQ, ONEQ, ONEQ}, // C
+  {THRQ, EXTD, HALF, HALF, HALF}, // D
+  {BENT, THRQ, THRQ, THRQ, THRQ}, // E
+  {THRQ, THRQ, EXTD, EXTD, EXTD}, // F
+  {HALF, EXTD, BENT, BENT, BENT}, // G
+  {HALF, EXTD, EXTD, BENT, BENT}, // H
+  {THRQ, BENT, BENT, BENT, EXTD}, // I
+  {THRQ, BENT, BENT, BENT, EXTD}, // J (needs motion)
+  {EXTD, EXTD, EXTD, BENT, BENT}, // k
+  {EXTD, EXTD, BENT, BENT, BENT}, // L
+  {BENT, BENT, BENT, BENT, BENT}, // M (order matters)
+  {BENT, BENT, BENT, BENT, BENT}, // N (order matters)
+  {HALF, THRQ, THRQ, THRQ, THRQ}, // O
+  {EXTD, EXTD, HALF, BENT, BENT}, // P
+  {EXTD, EXTD, BENT, BENT, BENT}, // Q
+  {THRQ, EXTD, EXTD, BENT, BENT}, // R
+  {BENT, BENT, BENT, BENT, BENT}, // S (order matters)
+  {BENT, ONEQ, BENT, BENT, BENT}, // T (order matters)
+  {BENT, EXTD, EXTD, BENT, BENT}, // U
+  {BENT, EXTD, EXTD, BENT, BENT}, // V (need lateral separation b/t one and two
+  {BENT, EXTD, EXTD, EXTD, BENT}, // W (need lateral separation b/t one and two and three
+  {BENT, ONEQ, BENT, BENT, BENT}, // X
+  {EXTD, BENT, BENT, BENT, EXTD}, // Y
+  {HALF, EXTD, BENT, BENT, BENT}  // Z (needs motion)
 };
 
 void setup() {
@@ -58,20 +64,35 @@ void setup() {
   Serial.println("Began the serial at 9600");
   
   // Some kind of setup procedure
-  driveFinger(0, SERVO_MAX_POS);
-  delay(2000);  
-  driveFinger(0, 90);
+  for (int i = 0; i < 5; i++) {
+    driveFinger(i, SERVO_MAX_POS);
+  }
+  
+//  delay(2000);
+  
+  for (int i = 0; i < 5; i++) {
+    driveFinger(i, SERVO_MIN_POS);
+  }
+  
   delay(2000);
-  driveFinger(0, SERVO_MIN_POS);
+    
+  for (int i = 0; i < 5; i++) {
+    driveFinger(i, SERVO_MAX_POS);
+  }
+ 
+  Serial.println("Set up at min pos");
+  
+  Serial.println("Ready to go!");
+  
+  
     
 }
-
+int cmd;
 
 int command[2];
 
 void loop() {  
   String msg = "";
-  
   while (Serial.available() > 0) {
     char incomingChar = (char) Serial.read();    
     msg.concat(incomingChar);
@@ -96,12 +117,34 @@ void loop() {
     Serial.println(msg);
     teach(msg.charAt(0));
   }
+  
+//    // Wait for and get position command
+//  Serial.print(">>");                            // Prompt next command
+//  while (!Serial.available()) continue;
+//  cmd = Serial.parseInt();
+//  Serial.println("");
+//  Serial.println(cmd);              // Echo command
+//  if (cmd > 180 || cmd < 0) goto endloop;        // If not valid command, break
+//  if (cmd == 180) cmd = 179;                       // Remap cmd = 180 value (technically invalid) to correct value for UX
+//  drive(cmd);                  // Else, valid command --> go to position
+//  
+//  Serial.print("Index finger is now at: ");
+//  Serial.println(cmd);
+//
+//endloop:
+//    Serial.println("");
    
   // Looping to test servo 
 //  fingers[0].write(SERVO_MAX_POS);
 //  delay(2000);
 //  fingers[0].write(SERVO_MIN_POS);
 //  delay(2000);
+  
+}
+
+void drive(int cmd) {
+  fingers[0].write(cmd); // pin9
+  delay(1000);
   
 }
 
